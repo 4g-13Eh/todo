418852b5280beb5ee30c9af92bb52914b5f94dbb
Shahin Amon <amons@bzz.ch>
2024-07-04 18:18:30 +0200

    add Textinputfield in all_tasks_page

--- lib/pages/all_tasks_page.dart
+++ lib/pages/all_tasks_page.dart
@@ -12,28 +12,59 @@ class AllTasksPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('ToDo App'), backgroundColor: Color.fromARGB(255, 188, 245, 56)),
-      body: Container(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          children: [
-            searchField(),
-            Expanded(
-              child: ListView(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(top: 20, bottom: 20),
-                    child: Text('Alle Aufgaben', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold), textAlign: TextAlign.center,),
+      body: Stack(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              children: [
+                searchField(),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(top: 20, bottom: 20),
+                        child: Text('Alle Aufgaben', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold), textAlign: TextAlign.center,),
+                      ),
+                      for( ToDo todo in todoList)
+                        ToDoItem(todo: todo)
+                    ],
+                  )
+                )
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(children: [
+              Expanded(child: Container(
+                margin: EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 255, 255, 255),
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: const [BoxShadow(color: Color.fromARGB(255, 46, 131, 216), blurRadius: 5)]
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(10),
+                    hintText: 'Neue Aufgabe',
                   ),
-                  for( ToDo todo in todoList)
-                    ToDoItem(todo: todo)
-                ],
-              )
+                ),
+              ),),
+              Container(
+                margin: EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 46, 131, 216),
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: const [BoxShadow(color: Color.fromARGB(255, 46, 131, 216), blurRadius: 5)]
+                ),
+                child: IconButton(
+                  icon: const Icon(Icons.add, color: Colors.white,),
+                  onPressed: (){},
+                ),)
+            ],),
             )
-            
-            
-          ],
-        ),
-        
+        ],
       ),
       backgroundColor: Color.fromARGB(255, 255, 255, 255),
     );
