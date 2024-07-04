edf5a6c39deb92d4e05156fafde0911be20c85d3
Shahin Amon <amons@bzz.ch>
2024-07-04 16:34:15 +0200

    updated homepage

--- lib/pages/all_tasks_page.dart
+++ lib/pages/all_tasks_page.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:todo/widgets/todo_item.dart';
+import 'package:todo/model/todo.dart';
 
 class AllTasksPage extends StatelessWidget {
   const AllTasksPage({Key? key}) : super(key: key);
+
+  final todoList = ToDo.toDos;
 
   @override
   Widget build(BuildContext context) {
@@ -13,6 +17,20 @@ class AllTasksPage extends StatelessWidget {
         child: Column(
           children: [
             searchField(),
+            Expanded(
+              child: ListView(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 20, bottom: 20),
+                    child: Text('Alle Aufgaben', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold), textAlign: TextAlign.center,),
+                  ),
+                  ToDoItem(),
+                  for( ToDo todo in todoList)
+                    ToDoItem(todo: todo,)
+                ],
+              )
+            )
+            
             
           ],
         ),
